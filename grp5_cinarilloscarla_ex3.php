5daa40c6ccc8e6c2f36af3b7b084843fd37c2cab
cai9253 <ccinarillos@gmail.com>
2024-09-12 16:47:28 +0800

    Update grp5_cinarilloscarla_ex3.php

--- grp5_cinarilloscarla_ex3.php
+++ grp5_cinarilloscarla_ex3.php
@@ -3,20 +3,21 @@ $fileName = 'sample.txt';
 
 
 if (file_exists($fileName)) {
-    echo "Carla Cinarillos.\n";
+    echo "Carla Cinarillos.<br>";
     
     
  file_get_contents($fileName);
+    $fileContents = file_get_contents($fileName);
     if ($fileContents === false) {
         echo "Error reading the file.\n";
     } else {
         echo "File contents:\n";
-        echo $fileContents . "\n";  
+        echo $fileContents . "<br>";  
 
 
         $lines = file($fileName, FILE_IGNORE_NEW_LINES);
         if ($lines === false) {
-            echo "Error reading the file lines.\n";
+            echo "Error reading the file lines.<br>";
         } else {
             echo "File lines:\n";
             foreach ($lines as $line) {
@@ -25,20 +26,19 @@ if (file_exists($fileName)) {
         }
     }
 } else {
-    echo "File does not exist.\n";
+    echo "File does not exist.<br>";
     
 
     $initialContent = "This is the initial content of the file.\n";
     $result = file_put_contents($fileName, $initialContent);
     if ($result === false) {
-        echo "Error writing to the file.\n";
+        echo "Error writing to the file.<br>";
     } else {
-        echo "File created and content written.\n";
+        echo "File created and content written.<br>";
         
-
-        $fileContents = file_get_contents($fileName);
+        
         if ($fileContents === false) {
-            echo "Error reading the file.\n";
+            echo "Error reading the file.<br>";
         } else {
             echo "New file contents after creation:\n";
             echo $fileContents . "\n";
